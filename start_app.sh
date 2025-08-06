bb95af7558f8c401f314dff01646797293df5a55
Jonathan Meixueiro <lav.jonathanom@gmail.com>
2025-08-06 00:26:10 -0600

    Correction for venv directory for linux/macOS

--- start_app.sh
+++ start_app.sh
@@ -27,11 +27,11 @@ if [ ! -d "venv" ]; then
     read -p "¿Deseas crearlo y instalar dependencias? (s/n): " crear_venv
     if [[ "$crear_venv" == "s" ]]; then
         python -m venv venv
-        source venv/Scripts/activate
+        source venv/bin/activate
         pip install -r requirements.txt
     fi
 else
-    source venv/Scripts/activate
+    source venv/bin/activate
 fi
 
 # Verificar archivo .env
