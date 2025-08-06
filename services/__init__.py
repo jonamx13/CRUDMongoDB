582ce4e789894c81b1c01b49f28d1d00f52ea5e0
Jonathan Meixueiro <lav.jonathanom@gmail.com>
2025-08-06 00:15:57 -0600

    Export services modularized into packages

--- services/__init__.py
+++ services/__init__.py
@@ -1,11 +1,11 @@
 """
 Paquete que contiene todos los servicios CRUD
 """
-from .create_service import crear_empleado
-from .read_service import leer_empleados
-from .update_service import actualizar_empleado
-from .delete_service import eliminar_empleado
-from .search_service import buscar_empleado
+from .create.service import crear_empleado
+from .read.service import leer_empleados
+from .update.service import actualizar_empleado
+from .delete.service import eliminar_empleado
+from .search.service import buscar_empleado
 
 # Exportar servicios para fácil acceso
 __all__ = [
